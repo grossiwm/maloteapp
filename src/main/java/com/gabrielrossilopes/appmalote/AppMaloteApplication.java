00702c0a859545be99f9fdb087f67d574e1deadf
gabriel <grossiwm@gmail.com>
2021-11-27 17:54:25 -0300

    Configuração para jsp

--- src/main/java/com/gabrielrossilopes/appmalote/AppMaloteApplication.java
+++ src/main/java/com/gabrielrossilopes/appmalote/AppMaloteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppmaloteApplication {
+public class AppMaloteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppmaloteApplication.class, args);
+		SpringApplication.run(AppMaloteApplication.class, args);
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title> Pagina JSP Ola Mundo</title>
+    </head>
+    <body>
+        <h1>
+            Ola Mundo
+        </h1>
+    </body>
+</html>
